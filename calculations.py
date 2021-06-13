b8bdd4561b0cd5adf6d8ae0c2fe85cbb8f4f41fc
Vaughn Roper <vaughnalanroper@gmail.com>
2021-06-13 15:51:25 -0400

    added general outline beginnings

--- calculations.py
+++ calculations.py
@@ -1,4 +1,18 @@
+import math
 # take 2 inputs - one starting angle and one final angle.
 # program should output the matrix that encodes for the transformation
 start = input('enter starting angle')
 final = input('enter angle that transformation should result in')
+# the two types of transforms involved in this should be : flip across cener of
+# unit circle, rotate around unit circle
+
+# step one: convert angle to raydians
+# step 2 : convert raydians to vecor
+# startRaydian = [cos(angle), sin(angle)]
+
+
+def degreeToRaydians(x):
+    (x*math.pi)/180
+
+
+print(degreeToRaydians(start))
