7c94deab2868725ca30c9eb4b230a9c9a5925d29
Vaughn Roper <vaughnalanroper@gmail.com>
2021-06-13 15:26:35 -0400

    test commit

---
+++ calculations.py
@@ -0,0 +1,4 @@
+# take 2 inputs - one starting angle and one final angle.
+# program should output the matrix that encodes for the transformation
+start = input('enter starting angle')
+final = input('enter angle that transformation should result in')
