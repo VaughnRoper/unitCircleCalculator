0e5161bb5fe282f52c61e8c500fde26d4158a03d
Vaughn Roper <vaughnalanroper@gmail.com>
2021-06-13 21:21:16 -0400

    finished the basic program
    on an unrelated note, #*(@&(#&@#&(@ inkscape for deciding that it should 
    be the global PYTHONPATH for my PC. what a waste of time!

--- calculations.py
+++ calculations.py
@@ -1,8 +1,10 @@
 import math
+import numpy as np
+from numpy import matrix
 # take 2 inputs - one starting angle and one final angle.
 # program should output the matrix that encodes for the transformation
-start = input('enter starting angle ')
-final = input('enter angle that transformation should result in ')
+start = float(input('enter starting angle '))
+final = float(input('enter angle that transformation should result in '))
 # the two types of transforms involved in this should be : flip across cener of
 # unit circle, rotate around unit circle
 # step one: convert angle to raydians
@@ -18,6 +20,11 @@ startRaydian = degreeToRaydians(start)
 finalRaydian = degreeToRaydians(final)
 startVector = [math.cos(startRaydian), math.sin(startRaydian)]
 finalVector = [math.cos(finalRaydian), math.sin(finalRaydian)]
-print(startVector)
+# start vector * matrix x = final vector. solve for matrix
+ScalarX = finalVector[0]/startVector[0]
+ScalarY = finalVector[1]/startVector[1]
+awnserMatrix = matrix([[ScalarX, 0], [0, ScalarY]])
+awnser = np.array(awnserMatrix)
+print('The matrix that encodes this transform is:', awnser, sep='\n')
 
 input('press enter to close')
