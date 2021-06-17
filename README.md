3318939a121854e74927df09374e7964611eb4ef
Vaughn Roper <vaughnalanroper@gmail.com>
2021-06-17 19:40:33 -0400

    Added UI

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # unitCircleCalculator
 
 For Calculating the matrix of transformations on the unit circle.
-Takes an angle from the start of a transformation and the angle the transform should end on, then outputs the matrix that encodes that transform.
+Takes an angle from the angleList[0]of a transformation and the angle the transform should end on, then outputs the matrix that encodes that transform.
 
 Requires the numpy library. install it by going to your command line and pasting 'pip install numpy'. You will also need python 3 or later installed.
 
--- calculations.py
+++ calculations.py
@@ -1,35 +1,55 @@
 import math
-import numpy as np
-from numpy import matrix
-# import needed libraries
-# take 2 inputs - one starting angle and one final angle.
-# program should output the matrix that encodes for the transformation between
-# these two angles
-start = float(input('enter starting angle '))
-final = float(input('enter angle that transformation should result in '))
-# convert angles to raydians
+# import numpy as np
+# from numpy import matrix
+import PySimpleGUI as sg
 
 
 def degreeToRaydians(x):
     return (x*math.pi)/180
 
 
-startRaydian = degreeToRaydians(start)
-finalRaydian = degreeToRaydians(final)
-# convert raydians to vectors by taking their sines and cosines
-startVector = [math.cos(startRaydian), math.sin(startRaydian)]
-finalVector = [math.cos(finalRaydian), math.sin(finalRaydian)]
-# start vector * matrix x = final vector. solve for matrix
-# find the scalars that when multiplied by the basis vectors, distort them in a
-# way that would turn startVector into finalVector
-ScalarX = finalVector[0]/startVector[0]
-ScalarY = finalVector[1]/startVector[1]
-# now assemble the matrix. scalarX goes in the top left slot, and scalarY goes
-# in the Bottom right. the other two slots are 0's, as they make up the
-# matrix's null space
-awnserMatrix = matrix([[ScalarX, 0], [0, ScalarY]])
-# format and print the matrix
-awnser = np.array(awnserMatrix)
-print('The matrix that encodes this transform is:', awnser, sep='\n')
-
-input('press enter to close')
+TimesOk = 0
+angleList = [0, 0]
+DisplayText = 'enter starting angle'
+layout = [
+    [sg.Text('enter starting angle', size=(40, 1), key='-Directions-')],
+    [sg.Input(key='-Input-')],
+    [sg.Button('Ok'), sg.Button('Quit'), sg.Button('Reset')],
+    [sg.Text(size=(40, 1), key='-output1-')],
+    [sg.Text(size=(40, 1), key='-output2-')]]
+window = sg.Window("Calculate", layout)
+while True:
+    event, values = window.read()
+    if event == 'Ok':
+        angleList[TimesOk] = values['-Input-']
+        TimesOk += 1
+        DisplayText = 'enter angle that transformation should result in'
+        window['-Directions-'].update(DisplayText)
+        for i in range(int(TimesOk-1)):
+            startRaydian = degreeToRaydians(int(angleList[0]))
+            finalRaydian = degreeToRaydians(int(angleList[1]))
+            # convert raydians to vectors by taking their sines and cosines
+            startVector = [math.cos(startRaydian), math.sin(startRaydian)]
+            finalVector = [math.cos(finalRaydian), math.sin(finalRaydian)]
+            # start vector * matrix x = final vector. solve for matrix
+            # find the scalars that when multiplied by the basis vectors, distort them in a
+            # way that would turn startVector into finalVector
+            ScalarX = finalVector[0]/startVector[0]
+            ScalarY = finalVector[1]/startVector[1]
+            # now assemble the matrix. scalarX goes in the top left slot, and scalarY goes
+            # in the Bottom right. the other two slots are 0's, as they make up the
+            # matrix's null space
+            topLine = '[ ' + str(ScalarX) + ', 0 ]'
+            bottomLine = '[ 0, ' + str(ScalarY) + ' ]'
+            # format and print the matrix
+            window['-output1-'].update(topLine)
+            window['-output2-'].update(bottomLine)
+    if event == 'Reset':
+        TimesOk = 0
+        angleList = [0, 0]
+        DisplayText = 'enter starting angle'
+        window['-Directions-'].update(DisplayText)
+        window['-Input-'].update(value=None)
+    if event == 'Quit' or event == sg.WIN_CLOSED:
+        break
+window.close()
